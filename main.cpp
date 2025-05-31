d0528e47239c8d436393c4328454d76f94ce6a84
Szefix <kamil.kedra2004@gmail.com>
2025-05-31 22:23:23 +0200

    Big Update
    dodanie plikow dll z bilbioteki sfml w przypadku gdy ktos nie ma skonfigurowanych srodowisk

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+#include <windows.h>
 #include "SnakeGame.hpp"
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
 
+
 int main() {
+    SetDllDirectory("SFML");
     srand(static_cast<unsigned int>(time(0)));
     SnakeGame game;
     game.run();
